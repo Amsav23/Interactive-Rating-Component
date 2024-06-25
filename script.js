ea9f174ecfffd4178630f44da6aa13972f923606
Amyln <amsavarese23@gmail.com>
2024-06-25 15:45:05 -0400

    Update with Pam

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
+/* Create references to elements that I will use */
+const ratingState = document.getElementById("rating-state");
+const thankYouState = document.getElementById("thank-you-state");
+const ratingButtons = document.querySelectorAll(".rating-btn");
+console.log(ratingButtons)
+const submitButton = document.getElementById("submit-btn");
+const ratingValueSpan = document.getElementById("rating-value");
+let selectedRating = "";
+
+submitButton.addEventListener("click", () => {
+    console.log("submit")
+    if (selectedRating !="") {
+            // I want to show the thank-you-state div
+            // That means REMOVE the hidden class
+            thankYouState.classList.toggle("hidden")
+            // I need the rating-state div to be hidden
+            ratingState.classList.add("hidden")
+}
+})
+
+ratingButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        console.log("click")
+        // save the button text
+        selectedRating = button.textContent;
+        console.log(selectedRating)
+    })
+})
+//
+/*
 document.addEventListener("DOMContentLoaded", () => {
+    console.log("page loaded")
     const ratingState = document.getElementById("rating-state");
     const thankYouState = document.getElementById("thank-you-state");
     const ratingButtons = document.querySelectorAll(".rating-btn");
@@ -25,4 +56,4 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
-  
+*/  
